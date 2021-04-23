f81d4c19a2ef8d345d43b2902e24016539019688
Hexi1997 <18715156450@163.com>
2021-04-23 12:49:39 +0800

    6 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>App
-
-    
-  </div>;
+  return <div>                       App</div>;
 }
 
 export default App;
